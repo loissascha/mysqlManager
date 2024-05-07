45addbe64666a939e90b31a030ee2ae5f6eb281a
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-07 16:06:08 +0200

    import dump

--- MySqlManager/MySqlManager/Services/MySqlDumpService.cs
+++ MySqlManager/MySqlManager/Services/MySqlDumpService.cs
@@ -26,7 +26,7 @@ public abstract class MySqlDumpService
         return process.ExitCode == 0; // this checks if it's available on unix machines
     }
     
-    public static void DumpDatabase(string host, int port, string user, string password, string database)
+    public static void DumpDatabase(string host, string port, string user, string password, string database)
     {
         var settingsPath = SettingsService.GetSettingsFolderPath();
         var filepath = Path.Combine(settingsPath, "dump_" + database + ".sql");
@@ -35,7 +35,7 @@ public abstract class MySqlDumpService
         process.StartInfo.FileName = "mysqldump";
         process.StartInfo.RedirectStandardInput = false;
         process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.Arguments = $"--host={host} --user={user} --password={password} {database}";
+        process.StartInfo.Arguments = $"--host={host} --user={user} --password={password} --port={port} {database}";
         process.StartInfo.UseShellExecute = false;
 
         process.Start();
@@ -50,7 +50,7 @@ public abstract class MySqlDumpService
         process.WaitForExit();
     }
     
-    public static void DumpTable(string host, int port, string user, string password, string database, string table)
+    public static void DumpTable(string host, string port, string user, string password, string database, string table)
     {
         var settingsPath = SettingsService.GetSettingsFolderPath();
         var filepath = Path.Combine(settingsPath, "dump_" + database + "_" + table + ".sql");
@@ -59,7 +59,7 @@ public abstract class MySqlDumpService
         process.StartInfo.FileName = "mysqldump";
         process.StartInfo.RedirectStandardInput = false;
         process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.Arguments = $"--host={host} --user={user} --password={password} {database} {table}";
+        process.StartInfo.Arguments = $"--host={host} --user={user} --password={password} --port={port} {database} {table}";
         process.StartInfo.UseShellExecute = false;
 
         process.Start();
@@ -69,6 +69,26 @@ public abstract class MySqlDumpService
             string result = reader.ReadToEnd();
 
             File.WriteAllText(filepath, result);
+        }
+
+        process.WaitForExit();
+    }
+    
+    public static void MySqlImport(string host, string port, string user, string password, string database, string filepath)
+    {
+        using Process process = new Process();
+        process.StartInfo.FileName = "mysql";
+        process.StartInfo.RedirectStandardInput = true; 
+        process.StartInfo.RedirectStandardOutput = false;
+        process.StartInfo.Arguments = $"--host={host} --user={user} --password={password} --port={port} {database}";
+        process.StartInfo.UseShellExecute = false;
+
+        process.Start();
+
+        using (StreamWriter writer = process.StandardInput)
+        {
+            string fileContent = File.ReadAllText(filepath);
+            writer.Write(fileContent);
         }
 
         process.WaitForExit();
