d76ba92593d854349993bba31a7d060fc8861ae7
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-22 17:03:43 +0200

    top row design changes

--- MySqlManager/MySqlManager/Services/TopRowService.cs
+++ MySqlManager/MySqlManager/Services/TopRowService.cs
@@ -29,7 +29,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueBrowse = $"/database/{database}/table/{table}";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary ms-0 me-2 mt-3 {(active == "browse" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary ms-0 me-2 mt-3 {(active == "browse" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueBrowse);
             _builder.AddContent(++seq, "Browse");
             _builder.CloseElement();
@@ -39,7 +39,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueInformation = $"/database/{database}/table/{table}/information";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary ms-0 me-2 mt-3 {(active == "information" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary ms-0 me-2 mt-3 {(active == "information" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueInformation);
             _builder.AddContent(++seq, "Information");
             _builder.CloseElement();
@@ -49,7 +49,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueSql = $"/database/{database}/table/{table}/sql";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary me-2 mt-3 {(active == "sql" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary me-2 mt-3 {(active == "sql" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueSql);
             _builder.AddContent(++seq, "SQL");
             _builder.CloseElement();
@@ -59,7 +59,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueSearch = $"/database/{database}/table/{table}/search";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary me-2 mt-3 {(active == "search" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary me-2 mt-3 {(active == "search" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueSearch);
             _builder.AddContent(++seq, "Search");
             _builder.CloseElement();
@@ -69,7 +69,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueInsert = $"/database/{database}/table/{table}/insert";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary mt-3 {(active == "insert" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary mt-3 {(active == "insert" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueInsert);
             _builder.AddContent(++seq, "Insert");
             _builder.CloseElement();
@@ -96,7 +96,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueOverview = $"/database/{database}";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary ms-0 me-2 mt-3 {(active == "overview" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary ms-0 me-2 mt-3 {(active == "overview" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueOverview);
             _builder.AddContent(++seq, "Overview");
             _builder.CloseElement();
@@ -106,7 +106,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueImport = $"/database/{database}/import";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary me-2 mt-3 {(active == "import" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary me-2 mt-3 {(active == "import" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueImport);
             _builder.AddContent(++seq, "Import");
             _builder.CloseElement();
@@ -116,7 +116,7 @@ public class TopRowService
             _builder.OpenElement(++seq, "li");
             _builder.OpenElement(++seq, "a");
             var hrefValueExport = $"/database/{database}/export";
-            _builder.AddAttribute(++seq, "class", $"btn btn-primary me-2 mt-3 {(active == "export" ? "active" : "")}");
+            _builder.AddAttribute(++seq, "class", $"btn btn-outline-secondary me-2 mt-3 {(active == "export" ? "active" : "")}");
             _builder.AddAttribute(++seq, "href", hrefValueExport);
             _builder.AddContent(++seq, "Export");
             _builder.CloseElement();
