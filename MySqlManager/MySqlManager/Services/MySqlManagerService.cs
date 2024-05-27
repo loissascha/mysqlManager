1361127ba740cb4fc093843d8ccbb128a96f930a
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-05-27 20:25:55 +0200

    removed comment

--- MySqlManager/MySqlManager/Services/MySqlManagerService.cs
+++ MySqlManager/MySqlManager/Services/MySqlManagerService.cs
@@ -8,8 +8,6 @@ public class MySqlManagerService(
     DatabaseConnectionService _databaseConnectionService,
     DatabaseInformationService _databaseInformationService)
 {
-    //private const string ConnectionString = "server=localhost;port=30306;user=root;password=root";
-
     public async void Init()
     {
         Console.WriteLine("MySqlManagerService Init...");
